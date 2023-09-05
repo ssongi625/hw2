c2ae06926c9f452c412fbc78a29ba1355723a344
ssongi625 <ssongi625@naver.com>
2023-09-05 11:15:26 +0900

    add count.cc

--- count.cc
+++ count.cc
@@ -3,4 +3,16 @@
 void count_number() {
     // lab2 //
     // do it yourself //
+    using namespace std;
+    int count;
+    while (count < 11)
+    {
+	    cout << count;
+	    cout << "\n";
+    }
+
+    cout << "2019312742";
+    cout << endl;
+
+    return 0;
 }
